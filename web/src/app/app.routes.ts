2dde07c8b12f415781101d783c475dd3627c92e3
Charlie <charliegrillo@gmail.com>
2025-11-07 15:44:08 -0400

    fix: ajustar html y proveedores

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TeamListComponent } from './features/teams/team-list/team-list.component';
+import { StandingsListComponent } from './features/standings/standings-list/standings-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'teams', component: TeamListComponent },
+  { path: 'standings', component: StandingsListComponent }
+];
