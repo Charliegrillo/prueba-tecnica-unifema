d928fa4687aab8f16c790fa4b1826e2a450359ca
Charlie <charliegrillo@gmail.com>
2025-11-07 15:34:54 -0400

    feat: registrar servicios faltantes

--- backend/bootstrap/providers.php
+++ backend/bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
+    Laravel\Tinker\TinkerServiceProvider::class,
+    App\Providers\RepositoryServiceProvider::class
 ];
